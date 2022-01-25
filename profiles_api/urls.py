0b6834e5b6334090ae0bc2c2763a9f147119b7c5
Nikhil Badyal <nikhill773384@gmail.com>
2022-01-25 23:26:40 +0530

    :sparkles: Added viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloAPIViewSets, basename='hello-viewset')
+
 urlpatterns = [
-    path('hello-view/',views.HelloAiView.as_view())
+    path('hello-view/', views.HelloAPIView.as_view()),
+    path('', include(router.urls))
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,7 +7,50 @@ from profiles_api import serializers
 
 
 # noinspection PyMethodMayBeStatic
-class HelloAiView(APIView):
+class HelloAPIViewSets(viewsets.ViewSet):
+    """Test API Viewset"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions(list, create, retrieve, update, partial_update)'
+            'Automatically maps to URLs',
+            'More features , less code'
+        ]
+        return Response({'message': 'Hello', 'an_apiview': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hellow {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Retrieve an particular obj"""
+        return Response({'method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Update an object"""
+        return Response({'method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Update a field in object"""
+        return Response({'method': 'PATCH'})
+
+    def destroy(self, requests, pk=None):
+        """delete an object"""
+        return Response({'method': 'DELETE'})
+
+
+# noinspection PyMethodMayBeStatic
+class HelloAPIView(APIView):
     """Test API View"""
 
     serializer_class = serializers.HelloSerializer
