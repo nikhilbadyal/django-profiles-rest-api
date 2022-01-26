261e0678b61f1fcdb100bdd794f19a89278d883c
Nikhil Badyal <nikhill773384@gmail.com>
2022-01-26 12:10:36 +0530

    :sparkles: Added Searching

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,4 @@
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -100,3 +99,5 @@ class UserViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
