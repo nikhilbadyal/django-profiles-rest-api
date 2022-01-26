a121a1cab9a068b70060d24836e77612bbc40d67
Nikhil Badyal <nikhill773384@gmail.com>
2022-01-26 13:47:51 +0530

    :sparkles: Added Auth

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,5 +8,6 @@ router.register('hello-viewset', views.HelloAPIViewSets, basename='hello-viewset
 router.register('profile', views.UserViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloAPIView.as_view()),
+    path('login/', views.UserLoginAPIView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, filters, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 
 from profiles_api import permissions
@@ -101,3 +103,8 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    """Handling creating user auth"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
